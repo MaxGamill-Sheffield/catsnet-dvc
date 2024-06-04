7a1ba45652c0b34810e3294be9f6796f27563b7b
Max Gamill <mcgamill1@sheffield.ac.uk>
2024-06-04 13:27:40 +0100

    add augmentations into training

--- src/train.py
+++ src/train.py
@@ -4,6 +4,10 @@ from PIL import Image
 from loguru import logger
 from datetime import datetime
 import re
+from functools import partial
+import albumentations as alb
+from albumentations.core.transforms_interface import ImageOnlyTransform
+
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,6 +21,90 @@ from unet import unet_model
 
 yaml = YAML(typ="safe")
 
+# Create normalisation augmentation classes
+class ClipNormalise(ImageOnlyTransform):
+    """
+    Clip image transformation and noramlise its values for AFM images.
+    """
+    def __init__(self, safe_db_lists=[], p=1.0, lower_clip=None, upper_clip=None) -> None:
+        super(ClipNormalise, self).__init__()
+        self.safe_db_lists = safe_db_lists
+        self.p = p
+        self.lower_clip = lower_clip
+        self.upper_clip = upper_clip
+
+    def apply(self, img, copy=True, **params):
+        if np.random.uniform(0, 1) > self.p:
+            return img
+        if copy:
+            img = img.copy()
+        # clipping function
+        img = np.clip(img, self.lower_clip, self.upper_clip)
+        img -= self.lower_clip
+        img /= (self.upper_clip - self.lower_clip)
+        return img
+
+class ClipNormaliseMean(ImageOnlyTransform):
+    """
+    Clip image transformation and noramlise its values for AFM images.
+    """
+    def __init__(self, safe_db_lists=[], p=1.0, lower_clip=None, upper_clip=None) -> None:
+        super(ClipNormaliseMean, self).__init__()
+        self.safe_db_lists = safe_db_lists
+        self.p = p
+        self.lower_clip = lower_clip
+        self.upper_clip = upper_clip
+
+    def apply(self, img, copy=True, **params):
+        if np.random.uniform(0, 1) > self.p:
+            return img
+        if copy:
+            img = img.copy()
+        # clipping function
+        img = np.clip(img, self.lower_clip, self.upper_clip)
+        img -= img.mean()
+        img /= np.var(img)
+        return img
+
+def compile_augmentations(augmentations: dict) -> alb.core.composition.Compose:
+    """Collects augmentation functions and parameters from the config file into an albumenations object.
+
+    Parameters
+    ----------
+    augmentations : dict
+        Dictionary deffining the augmentation and it's associated parameters. The augmentation / key must
+        be present in the `augmentation_keys` variable below.
+
+    Returns
+    -------
+    albumentations.core.composition.Compose
+        A composed list of augmentations to apply to the data.
+    """
+    augmentation_keys = {
+        "clip_and_normalise": partial(ClipNormalise),
+        "clip_and_mean_normalise": partial(ClipNormaliseMean),
+        "random_crop": partial(alb.RandomCrop),
+        "horrizontal_flip": partial(alb.HorizontalFlip),
+        "vertical_flip": partial(alb.VerticalFlip),
+        "rotate_90": partial(alb.RandomRotate90),
+        "brightness_contrast": partial(alb.RandomBrightnessContrast),
+        "blur": partial(alb.Blur),
+        "gauss_noise": partial(alb.GaussNoise),
+        "elastic": partial(alb.ElasticTransform),
+        }
+    augmentation_functions = []
+    for augmentation, params in augmentations.items():
+        if params.pop('run'):
+            # append to the tranform sequence
+            try:
+                augmentation_functions.append(augmentation_keys[augmentation](**params))
+            except KeyError as e:
+                logger.info(f"KeyError: {e} not in available augmentations")
+        else:
+            logger.info(f"Augmentation: {augmentation} is turned off")
+    
+    return alb.Compose(augmentation_functions)
+
 
 # generator for data
 def image_data_generator(
@@ -24,8 +112,7 @@ def image_data_generator(
     image_indexes: np.ndarray,
     batch_size: int,
     model_image_size: Tuple[int, int],
-    norm_upper_bound: float,
-    norm_lower_bound: float,
+    augmentations: dict,
 ):
     """Generate batches of images and ground truth masks."""
 
@@ -41,23 +128,20 @@ def image_data_generator(
             image = np.load(data_dir / f"image_{index}.npy")
             ground_truth = np.load(data_dir / f"mask_{index}.npy").astype(bool)
 
-            # TODO: Augment the images: Scale and translate
+            # Augment the images
+            transforms = compile_augmentations(augmentations)
+            transformed_data = transforms(image=image, mask=ground_truth)
+            trans_image = transformed_data['image']
+            trans_ground_truth = transformed_data['mask']
 
             # Resize without interpolation
-            pil_image = Image.fromarray(image)
+            pil_image = Image.fromarray(trans_image)
             pil_image = pil_image.resize(model_image_size, resample=Image.NEAREST)
             image = np.array(pil_image)
 
-            pil_ground_truth = Image.fromarray(ground_truth)
+            pil_ground_truth = Image.fromarray(trans_ground_truth)
             pil_ground_truth = pil_ground_truth.resize(model_image_size, resample=Image.NEAREST)
             ground_truth = np.array(pil_ground_truth)
-
-            # Normalise the image
-            image = np.clip(image, norm_lower_bound, norm_upper_bound)
-            image = image - norm_lower_bound
-            image = image / (norm_upper_bound - norm_lower_bound)
-
-            # TODO: Augment images: Flipping and rotate
 
             # Add the image and ground truth to the batch
             batch_input.append(image)
@@ -81,9 +165,8 @@ def train_model(
     activation_function: str,
     learning_rate: float,
     batch_size: int,
+    augmentation_params: dict,
     epochs: int,
-    norm_upper_bound: float,
-    norm_lower_bound: int,
     validation_split: float,
 ):
     """Train a model to segment images."""
@@ -99,8 +182,6 @@ def train_model(
     logger.info(f"|  Learning rate: {learning_rate}")
     logger.info(f"|  Batch size: {batch_size}")
     logger.info(f"|  Epochs: {epochs}")
-    logger.info(f"|  Normalisation upper bound: {norm_upper_bound}")
-    logger.info(f"|  Normalisation lower bound: {norm_lower_bound}")
     logger.info(f"|  Test size: {validation_split}")
 
     # Set the random seed
@@ -130,8 +211,7 @@ def train_model(
         image_indexes=train_indexes,
         batch_size=batch_size,
         model_image_size=model_image_size,
-        norm_upper_bound=norm_upper_bound,
-        norm_lower_bound=norm_lower_bound,
+        augmentations=augmentation_params,
     )
 
     validation_generator = image_data_generator(
@@ -139,8 +219,7 @@ def train_model(
         image_indexes=validation_indexes,
         batch_size=batch_size,
         model_image_size=model_image_size,
-        norm_upper_bound=norm_upper_bound,
-        norm_lower_bound=norm_lower_bound,
+        augmentations=augmentation_params,
     )
 
     # Load the model
@@ -224,7 +303,6 @@ if __name__ == "__main__":
         learning_rate=train_params["learning_rate"],
         batch_size=train_params["batch_size"],
         epochs=train_params["epochs"],
-        norm_upper_bound=train_params["norm_upper_bound"],
-        norm_lower_bound=train_params["norm_lower_bound"],
+        augmentation_params = train_params["augmentations"],
         validation_split=train_params["validation_split"],
     )
